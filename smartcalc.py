c527bb3005a9a3d9bf2625a48bc75dc10077618d
kanar11 <kanarekkasper@gmail.com>
2026-05-28 15:05:42 +0100

    Added subtraction function

--- smartcalc.py
+++ smartcalc.py
@@ -3,3 +3,6 @@
 # Addition function - adds two numbers and returns the result
 def add(a, b):
     return a + b
+# Subtraction function - subtracts b from a and returns the result
+def subtract(a, b):
+    return a - b
